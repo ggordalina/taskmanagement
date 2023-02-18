0376ac22a9e839becec6f46a9982ae88fe55cb4e
ggordalina <goncalo.jp.gordalina@gmail.com>
2023-02-18 17:49:22 +0000

    chore: update user and user roles communication

--- respositories/userRoleRepository.js
+++ respositories/userRoleRepository.js
@@ -1,20 +1,32 @@
+const UserRole = require('../models/userRole');
+
 const userRoleRepository = (dbConnection) => {
     if (!dbConnection) {
         throw new Error('dbConnection cannot be empty.');
     }
+
+    const userRoleTableCollumns = [
+        'BIN_TO_UUID(Id) AS Id',
+        'Description'
+    ];
     
     const get = async (userRoleId) => {
         if (!userRoleId){
             throw new Error("userRoleId cannot be empty.");
         }
 
-        let sqlQuery = 'SELECT * FROM `UserRole` WHERE `Id` = ?';
+        let sqlQuery = `SELECT ${userRoleTableCollumns} FROM UserRole WHERE Id = ?`;
         let params = [userRoleId];
     
-        return await dbConnection.execute(sqlQuery, params);
+        let result = await dbConnection.execute(sqlQuery, params);
+        if (result?.length <= 0) {
+            return null;
+        }
+
+        return UserRole.map(result)[0];
     };
 
-    return { get }
+    return { get };
 };
 
 module.exports = userRoleRepository;
