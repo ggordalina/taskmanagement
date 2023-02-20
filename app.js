2097fabd0ea0fc486d4403f4d2cffea9cf7f8d89
ggordalina <goncalo.jp.gordalina@gmail.com>
2023-02-20 00:23:34 +0000

    chore: refactor project and requires

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const webApi = require('./web-api/webApi');
+const webApi = require('./application/webApi');
 
 const app = express();
 const port = process.env.APP_PORT;
